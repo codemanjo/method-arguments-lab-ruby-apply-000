59db5daff14505bf90420068d5e47fdf1d947022
JOSIAH E MODESTE <ephraimmo2297@gmail.com>
2020-01-18 06:50:16 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+
+
+def introduction_with_language(name,language)
+  puts"Hi my is name #{name[1]} and I am learning to program in #{language[2]}"
+end
+
+name =["Josiah","John","Jordan"]
+language =["Javasript","python","ruby"]
+introduction_with_language(name,language)
